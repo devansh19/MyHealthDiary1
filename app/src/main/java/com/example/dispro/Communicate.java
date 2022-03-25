7448dff4505d3f1b3589b5775f91c7896a7cca31
Deep-47 <deepanshuaman21@gmail.com>
2022-03-25 10:47:47 +0530

    changed name
    changes

--- app/src/main/java/com/example/dispro/Communicate.java
+++ app/src/main/java/com/example/dispro/Communicate.java
@@ -56,7 +56,7 @@ class SocketSend extends AsyncTask<String, Void, String> {
     String buffer = "";
     int inp=-2;
 
-    final private String ip="192.168.0.103";
+    final private String ip="192.168.246.177";
     final private int port = 5000;
 
     @Override
